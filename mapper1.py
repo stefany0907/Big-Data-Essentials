8e8f6cc4ce46cc3c38c8b28587c59355a1fec212
Matt Chen <maoter.chen@gmail.com>
2019-05-23 21:37:01 -0700

    WordGroupTask4 updated v1

--- mapper1.py
+++ mapper1.py
@@ -2,24 +2,22 @@
 import sys
 import re
 
-reload(sys)
-sys.setdefaultencoding('utf-8')  # required to convert to unicode
+path = "stop_words_en.txt"
 
-path = 'stop_words_en.txt'
-
-# Your code for reading stop words here
-with open(path, "r") as f:
-    stop_words = f.read().split('\n')
+with open(path, "r") as file:
+    stop_words = file.read().splitlines()
 
 for line in sys.stdin:
-    try:
-        article_id, text = unicode(line.strip()).split('\t', 1)
-    except ValueError as e:
-        continue
 
-    words = re.split("\W*\s+\W*", text, flags=re.UNICODE)
-    words = [word for word in words if (word not in stop_words) and word.isalpha()]
-    # Your code for mapper here.
-    for word in words:
-        print >> sys.stderr, "reporter:counter:Wiki stats,Total words,%d" % 1
-        print "%s\t%d" % (word.lower(), 1)
+    article_id, text = line.strip().split('\t', 1)
+
+    try:
+        words = re.split('\W*\s+\W*', text.strip())
+        words = [word for word in words if (word not in stop_words) and word.isalpha()]
+        
+        for word in words:
+            print("{}\t{}".format(word.lower(), 1))
+            
+    except Exception as e:
+        print(e)
+        continue
